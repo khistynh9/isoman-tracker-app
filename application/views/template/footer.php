edd86d36c97e593dac5840820e7cd03bc0d1e9ec
khistynh <khistynh@gmail.com>
2021-09-08 06:50:53 +0700

    ubah footer tahun

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -2,7 +2,7 @@
     <div class="pull-right hidden-xs">
         <b>Version</b> 0.1.0
     </div>
-    <strong>Copyright &copy; 2018 - <?php echo date('Y'); ?>.</strong> All rights
+    <strong>Copyright &copy; <?= date('Y'); ?>.</strong> All rights
     reserved.
 </footer>
 
